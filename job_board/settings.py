7b2282addd59486853f363f1eb4cf3560eb11cd7
omor faruk <md3147693@gmail.com>
2024-08-23 19:31:14 -0700

    csrf tokens add

--- job_board/settings.py
+++ job_board/settings.py
@@ -24,6 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!=689d840%qk)4r2at9b@xo-s=o7qwq$+#q0wo4co#+^kz-u%1'
 
+
+CSRF_TRUSTED_ORIGINS = ['https://job-board-backend-zxvu.onrender.com','https://*.127.0.0.1']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
