11f9475addc173db50e7a0c74cb42e05ceec5759
omor faruk <md3147693@gmail.com>
2024-08-26 23:53:27 -0700

    settings.py updated

--- job_board/settings.py
+++ job_board/settings.py
@@ -37,15 +37,13 @@ ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        # other authentication classes
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
 }
-
 
 # Application definition
 
