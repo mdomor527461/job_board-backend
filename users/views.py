975e1af2982f669e27fd73c5344e6bd9ae115809
omor faruk <md3147693@gmail.com>
2024-10-30 13:34:48 +0600

    d

--- users/views.py
+++ users/views.py
@@ -91,7 +91,7 @@ class UserLoginApiView(APIView):
             if user:
                 token, _ = Token.objects.get_or_create(user=user)
                 login(request, user)
-                return Response({'token': token.key, 'user_id': user.id,'user_type': user.user_type,'image_url':user.image_url})
+                return Response({'token': token.key, 'user_id': user.id,'user_type': user.user_type,'image_url':user.image_url,'is_premium':user.is_premium})
             else:
                 return Response({'error': "Invalid Credential"}, status=400)
         
